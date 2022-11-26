8ca402714b68271ea663b339671f8e96b42d2509
Ivan Gamanovich <igamanovich@elinext.com>
2022-11-26 16:57:22 +0300

    feat: add tooltip

--- prime-ng/src/app/app.module.ts
+++ prime-ng/src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {ButtonModule} from "primeng/button";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {InputTextModule} from "primeng/inputtext";
+import {FormsModule} from "@angular/forms";
+import {TooltipModule} from "primeng/tooltip";
 
 @NgModule({
   declarations: [
@@ -12,7 +15,11 @@ import {ButtonModule} from "primeng/button";
   imports: [
     BrowserModule,
     ButtonModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    InputTextModule,
+    AppRoutingModule,
+    TooltipModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
